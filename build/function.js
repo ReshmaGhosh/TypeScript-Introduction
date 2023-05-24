9f73626d9981723ce23acb3e64ef246716948ff6
Reshma-Ghosh <reshmaghosh114@gmail.com>
2023-05-24 12:59:56 +0200

    Typescript-intro

---
+++ build/function.js
@@ -0,0 +1,13 @@
+"use strict";
+// 1. write a function to check the length of user input as their name
+// 2. write a function to compare the number user insert to the number 3 then display the suitable message
+// the users array for assignment 3 and 4 below
+const users = [
+    { name: "andrea", age: 4 },
+    { name: "hanna", age: 9 },
+];
+// 3. write a function that take users array as a parameter then check if user has age greater than 3
+// 4. write a function takes 2 parameters : users array and userInput to check if the userInput is matched with the user name
+//  2 ways
+// searchName(users, "alex"); => alex is not in the users array
+// searchName(users, "andrea"); => andrea is in the users array
